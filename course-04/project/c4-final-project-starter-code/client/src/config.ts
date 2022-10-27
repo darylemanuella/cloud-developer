3ca19c3e352719afb948dcc9bace130b05f3a14e
darylemanuella <darylemanuella2001@gmail.com>
2022-10-27 12:13:05 +0100

    enfin soumission du projet 4 developer cloud

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,11 +1,11 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'i2sadsnjl5'
+const apiId = 'kyce9chivh'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
-  domain: 'dev-rqmxap7r.us.auth0.com',            // Auth0 domain
-  clientId: 'H5p9q6MlFEF5vOVyJkVVp2QKJa28XSUB',          // Auth0 client id
+  domain: 'dev-lgquuha6dd838dwk.us.auth0.com',            // Auth0 domain
+  clientId: 'Tobc3H2Z1xl36xuP470ETrpfq0JWk6wf',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
